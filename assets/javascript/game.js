73dd2a743439db1d8c3f92ecb6123f63f7ab4c0d
GJDeY <gregoryjdeyoung@u.northwestern.edu>
2018-08-05 23:52:26 -0500

    stuck linking .js file and out of ideas, need help asap!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,26 @@
+//array for random words
+var randomWord = ["cubs", "Baseball", "bat"];
+var guesses = 9;
+
+var wins = 0;
+var losses = 0;
+var i = true;
+var userGuess = document.write.onkeypress;
+// This function is run when key is pressed
 document.onkeyup = function (event) {
 
+    // Determines which key was pressed.
+    var userGuess = event.key;
+
 }
-addEventListener < onkeyup; any
+var computerGuess = randomWord.length;
+//replace random word with "_ _'s"
+document.querySelector(".wins").innerHTML = wins;
+}
+//document.write score into <td>
+//document score for loss into <tr>
+for (var i = 0, i <= randomWord.length, i++) {
+    var newGame = onkeyup
+    function(newGame)(onkeyup) {
+
+    }
